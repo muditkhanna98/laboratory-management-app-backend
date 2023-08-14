9194b0bffe9cf7bdfd5430f732e5ec657ee73b20
dennx2 <jdcht1228@gmail.com>
2023-08-14 13:30:09 -0400

    project creation

---
+++ src/main/java/com/humber/laboratorymgntappbackend/LaboratoryMgntAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.humber.laboratorymgntappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryMgntAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryMgntAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/humber/laboratorymgntappbackend/LaboratoryMgntAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.laboratorymgntappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryMgntAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
